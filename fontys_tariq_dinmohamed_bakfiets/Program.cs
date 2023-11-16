f63df812735a7bdf109bcd8a4e271ba5b8b0834d
Tariq <tariq9853@gmail.com>
2023-11-16 19:18:45 +0100

    Add: Biketype selector.

--- fontys_tariq_dinmohamed_bakfiets/Program.cs
+++ fontys_tariq_dinmohamed_bakfiets/Program.cs
@@ -1,20 +1,58 @@
-﻿namespace fontys_tariq_dinmohamed_bakfiets
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace fontys_tariq_dinmohamed_bakfiets
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            String some = Deeznuts.hello("Nuteh");
-            Console.WriteLine(some);
+            //I decided to switch it up, bakfietsen alone are boring.
+            Console.WriteLine("Hallo, Welkom bij V.D Brinckes Fietsen");
+            Console.WriteLine("Maak een keuze uit te volgende typen fietsen\n");
+            //Printout the different bike types from enum
+            Selector.PrintBikeTypes();
+            string read_user_input = Console.ReadLine();
+/*            if (type_of_bike == null || type_of_bike == "")
+            {
+                Console.WriteLine("Whoops! daar ging iets fout! Kies een nummer");
+            }
+            else
+            {
+                Console.WriteLine(type_of_bike);
+            }*/
         }
     }
 
-    internal class Deeznuts
+
+    internal class Selector
     {
-        public static String hello(string deez)
+        public enum BikeTypes
         {
-            return deez;
+            Stadfiets = 10,
+            Sprintfiets = 20,
+            Bakfiets = 30,
+            Mountainbike = 40
+        }
+
+        public static void PrintBikeTypes()
+        {
+            //Enumarate over enum biketypes and print each , including pricing.
+            foreach (BikeTypes bikeType in Enum.GetValues(typeof(BikeTypes)))
+            {
+                /**
+                 * Here, we iterate through the BikeTypes enum, one type at a time, 
+                 * and for each type, we retrieve its value (integer representation).
+                 * We then assign both the type's name and its associated value to 'bikeType'.
+                 * 
+                 * So the first call on biketype prints the enum name.
+                 * The second call on biketype prints the associated int(price).
+                 */
+
+                Console.WriteLine($"{bikeType}, Kosten: {(int)bikeType}");
+            }
         }
     }
+
+
 }
--- fontys_tariq_dinmohamed_bakfiets/obj/Debug/net8.0/fontys_tariq_dinmohamed_bakfiets.AssemblyInfo.cs
+++ fontys_tariq_dinmohamed_bakfiets/obj/Debug/net8.0/fontys_tariq_dinmohamed_bakfiets.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fontys_tariq_dinmohamed_bakfiets")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e0c0e10f2c7e1562b1c7fb20dc97494cde8e805")]
 [assembly: System.Reflection.AssemblyProductAttribute("fontys_tariq_dinmohamed_bakfiets")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fontys_tariq_dinmohamed_bakfiets")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
