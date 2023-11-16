08588b347cc58e2236c3e086ac8a4932a91942c3
Tariq <tariq9853@gmail.com>
2023-11-16 18:12:18 +0100

    First commit

---
+++ fontys_tariq_dinmohamed_bakfiets/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace fontys_tariq_dinmohamed_bakfiets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            String some = Deeznuts.hello("Nuteh");
+            Console.WriteLine(some);
+        }
+    }
+
+    internal class Deeznuts
+    {
+        public static String hello(string deez)
+        {
+            return deez;
+        }
+    }
+}
